d40d5e37cc9efbd87cdf777d7a5520d19760736d
Robin Stahl Warelius <107325879+robinwarelius@users.noreply.github.com>
2023-05-02 20:41:07 +0200

    login and register form

--- src/contexts/LoginProvider.jsx
+++ src/contexts/LoginProvider.jsx
@@ -3,11 +3,28 @@ export const LoginContext = createContext();
 
 const LoginProvider = (props) => {
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const email = event.target.elements.email.value;
     const password = event.target.elements.password.value;
+    const remember = event.target.elements.remember.value;
+    
+    const data = {email : email, password : password, remember : remember}
+    const url = ''
+
+    const response = await postData(url, data);
+    console.log(response);
   };
+
+  async function postData (url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type' : 'application/json' },
+        body : JSON.stringify(data)
+    });
+    return response.json();
+    };
+    
 
   return (
     <>
