773d57790fab4ac8470ec30251f0831069d346d7
manwees <mannewester@gmail.com>
2023-05-03 09:28:05 +0200

    Navigation

---
+++ src/components/views/cart/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navigation from '../../partials/navigation/Navigation'
+
+const Cart = () => {
+  return (
+    <>      <Navigation />
+    </>
+  )
+}
+
+export default Cart
