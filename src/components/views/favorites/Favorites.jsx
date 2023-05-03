b97c6fdc6fbdb493d22b1113d8ac73184b932e84
manwees <mannewester@gmail.com>
2023-05-03 09:28:37 +0200

    Navigation

---
+++ src/components/views/favorites/Favorites.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from '../../partials/navigation/Navigation'
+
+
+const Favorites = () => {
+  return (
+     <>      <Navigation />
+    </>
+  )
+}
+
+export default Favorites
