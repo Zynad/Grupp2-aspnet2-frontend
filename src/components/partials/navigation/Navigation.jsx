97465b1f1420e77d1e3f1afc8b339e874a1b4b1c
Ackeloo1 <axel.mattias.nyberg@gmail.com>
2023-05-17 14:08:32 +0200

    merge-redo

--- src/components/partials/navigation/Navigation.jsx
+++ src/components/partials/navigation/Navigation.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
       <div className="container">
       <div className='footer-nav container'>
       <nav>
-            <NavLink className="link" to="/"><i className="fa-2xs fa-light fa-house"></i></NavLink>
+            <NavLink className="link" to="/home"><i className="fa-2xs fa-light fa-house"></i></NavLink>
             <NavLink className="link" to="/search"><i className="fa-2xs fa-light fa-search"></i></NavLink>
             <NavLink className="link" to="/cart"><i className="fa-2xs fa-light fa-bag-shopping"></i></NavLink>
             <NavLink className="link" to="/favorites"><i className="fa-2xs fa-light fa-heart"></i></NavLink>
