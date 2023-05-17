4517958bc08914798704aebe33def37b2f486bf9
Robin Stahl Warelius <107325879+robinwarelius@users.noreply.github.com>
2023-05-17 14:13:03 +0200

    wishlist is completed

--- src/components/views/favorites/Favorites.jsx
+++ src/components/views/favorites/Favorites.jsx
@@ -4,6 +4,8 @@ import Navigation from '../../partials/navigation/Navigation'
 import {useContext} from 'react'
 import { WishlistContext } from '../../../contexts/WishlistProvider'
 import StarRating from './StarRating'
+import wishImg from "../../../assets/images/wishlist.png"
+import { NavLink } from 'react-router-dom'
 
 
 const Favorites = () => {
@@ -16,31 +18,45 @@ const Favorites = () => {
 
   const renderWishlist = () => {
 
-    return (    
-      <>    
-      {wishlist.map((item, index) => (
-        <div className="row wishlist-content">
+    if(wishlist.length != 0){
+      return (    
+        <>    
+        {wishlist.map((item, index) => (
+          <div className="row wishlist-content">
 
-        <div className="img-content">
-        <img className="img-wishlist" src={item.imageUrl}></img>
-        </div>
+          <div className="img-content">
+          <img className="img-wishlist" src={item.imageUrl}></img>
+          </div>
+          
+          <div className='col text-content'>
+          <div className='text'> {item.description}</div>
+          <div className='text'> ${item.price}</div>
+          <div className='text'><StarRating rating={item.rating}/></div> 
+          </div>
         
-        <div className='col text-content'>
-        <div className='text'> {item.description}</div>
-        <div className='text'> ${item.price}</div>
-        <div className='text'><StarRating rating={item.rating}/></div> 
-        </div>
-      
-        <div className="col icon-content">
-        <i class="fa-solid fa-heart icon-heart" onClick={() => {deleteProduct(item.id)}}></i>
-        </div>
-
-        <hr className='mt-4 mb-4'></hr>
-
-        </div> 
-      ))}
+          <div className="col icon-content">
+          <i class="fa-solid fa-heart icon-heart" onClick={() => {deleteProduct(item.id)}}></i>
+          </div>
+  
+          <hr className='mt-4 mb-4'></hr>
+          </div> 
+        ))}
+        </>
+      )
+    } else {
+      return (
+      <>
+      <div className='wishlist-empty-content'>
+      <img className='wishlist-empty' src={wishImg}></img>
+      <NavLink to="/"><div className='mt-5'><button id="button-wishlist-empty" className='dark-btn-standard'>SHOP NOW</button></div></NavLink>
+      </div>
       </>
-    )
+      )
+    }
+
+
+
+    
   }
 
   
@@ -49,7 +65,6 @@ const Favorites = () => {
      <div className='container mt-5'>
      {renderWishlist()}
      </div>
-     
      <Navigation />
     </>
   )
