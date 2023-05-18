3ae88306a5d2229500edafa8289140514a2957f3
Stefanie Schubert <stefanie.schubert@yh.nackademin.se>
2023-05-18 16:31:07 +0200

    updaterat kontaktinfo

--- src/components/views/contact/Contact.jsx
+++ src/components/views/contact/Contact.jsx
@@ -8,20 +8,51 @@ const Contact = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="contact-container">
       <div className="contact-icon" onClick={handleToggle}>
         <i className="fas fa-envelope"></i>
       </div>
-   
-        <div className={`contact-side-menu ${isOpen ? 'open' :''}`}>
+   {isOpen &&(
+        <div className="contact-side-menu">
           <div className="contact-menu-content">
-            <h3>Contact Information</h3>
-            <p>Location: Your location</p>
-            <p>Email: example@example.com</p>
-            <p>Phone: 123-456-7890</p>
+            <div className="navigation-arrow" onClick={handleClose}>
+                <i className="fas fa-arrow-left"></i>
+                </div>
+            <h3>Contact Us</h3>
+            <br></br>
+            <br></br>
+            <p className="location">
+                <i className="fas fa-map-marker-alt"></i> 27 Division St, New York.
+                <br></br>NY 10002, USA
+
+            </p>
+            <hr></hr>
+            <div className="email">
+                <a href="mailto:example@example.com">
+                <i className="fas fa-envelope"></i>
+                </a>
+                <p>manerosale@mail.com
+                    <br></br>
+                    manerosupport@mail.com
+                </p>
+            </div>
+            <hr></hr>
+
+            <p className="phone">
+                <i className="fas fa-phone"></i>+17 123456789
+                <br></br>
+                +17 987654321
+            </p>
+            <hr></hr>
+
           </div>
         </div>
+   )}
     </div>
   );
 };
