fcee1dc629b85960a995e8b63699ecc3a37bb5da
Ackeloo1 <axel.mattias.nyberg@gmail.com>
2023-05-24 18:35:00 +0200

    Home fixat och klart

--- src/components/views/home/Home.jsx
+++ src/components/views/home/Home.jsx
@@ -7,6 +7,7 @@ import { ApiContext } from '../../../contexts/ApiProvider'
 import { useState, useEffect } from 'react'
 import DetailedItem from '../../partials/shared/detailedItem/DetailedItem'
 import ColorSelector from '../../partials/shared/colorSelector/ColorSelector'
+import TopHeader from '../../partials/shared/topHeader/TopHeader'
 
 const Home = () => {
   const { getAllProductsAsync } = useContext(ApiContext);
@@ -35,6 +36,7 @@ const Home = () => {
   if (loadState) {
   return (
     <>
+        <TopHeader />
         <WelcomeBanner />
         <Collection title="Best sellers" itemList={bestSellerList} />
         <SaleBanner /> 
